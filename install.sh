eea29ba60a03495728ea4553f001e3d21f43fcc1
Jared <jared@digitalnutt.com>
2021-11-18 13:06:26 -0800

    updating vundle to plug for install

--- install.sh
+++ install.sh
@@ -28,9 +28,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 # Create dev directories
 mkdir $HOME/Dev
 
-# Install Vundle for Vim
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+# Install Plug for nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Create symlinks
 sh makesymlinks.sh
